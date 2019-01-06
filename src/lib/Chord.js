60dc9b45ff2c39780cac124673a335fa302d115e
Thiago Colares <thicolares@gmail.com>
2019-01-06 16:16:14 -0300

    Import and make a hello world with D3

--- src/lib/Chord.js
+++ src/lib/Chord.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import * as d3 from 'd3';
 
-const Chord = () => {
-    return <div>I'm the Chord component</div>
-};
+class Chord extends React.Component {
+    componentDidMount = () => {
+        d3.selectAll("div").style('background-color', 'red');
+    };
 
+    render() {
+        console.log('render');
+        return <div>I'm the Chord component</div>;
+    }
+}
 export default Chord;
