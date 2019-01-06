8fa8b4d925df3ddaa688ea50ef4f49bda07ce0b3
Thiago Colares <thicolares@gmail.com>
2019-01-06 13:56:31 -0300

    Create the Chord component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Chord!</div>, document.querySelector('#root'));
+import Chord from './lib/Chord';
+
+ReactDOM.render(<Chord/>, document.querySelector('#root'));
---
+++ src/lib/Chord.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Chord = () => {
+    return <div>I'm the Chord component</div>
+};
+
+export default Chord;
