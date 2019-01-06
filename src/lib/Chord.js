1d076103458579f44dc7fb6314ad974416e25eda
Thiago Colares <thicolares@gmail.com>
2019-01-06 17:01:08 -0300

    Make use of D3 correctly with React refs

--- src/lib/Chord.js
+++ src/lib/Chord.js
@@ -2,13 +2,29 @@ import React from 'react';
 import * as d3 from 'd3';
 
 class Chord extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.chordRef = React.createRef();
+    }
+
     componentDidMount = () => {
-        d3.selectAll("div").style('background-color', 'red');
+        console.log('component did mount');
+        console.log(this.chordRef.current);
+        d3.select(this.chordRef.current).style('color', 'red');
+
+        // It would safer to use this instead?
+        // this.chordRef.current.addEvenetListener('load', this.someLocalFunction);
+        // It MAKES the difference when we are talking about img, for instance
     };
 
     render() {
-        console.log('render');
-        return <div>I'm the Chord component</div>;
+        return (
+            <div>
+                <h3>Tilte of a chord</h3>
+                <div ref={this.chordRef}>Look, I'm the chord per se!</div>
+            </div>
+        );
     }
 }
 export default Chord;
