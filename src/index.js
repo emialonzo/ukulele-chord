debdd7ca0e80d7240dc463db9c302aeb5091f817
Thiago Colares <thicolares@gmail.com>
2019-01-11 21:06:08 -0300

    Improve chord examples

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import UkuleleChord from './lib/UkuleleChord';
 
 ReactDOM.render(
     <div>
-        <UkuleleChord name="C"  frets={[3, 0, 0, 0]}/>
-        <UkuleleChord name="Am" frets={[0, 0, 0, 2]}/>
-        <UkuleleChord name="G#" frets={[6, 4, 3, 5]}/>
+        <UkuleleChord name="C" frets={[3, 0, 0, 0]}/>
+        <UkuleleChord name="Db7+" frets={[3, 4, 5, 6]}/>
+        <UkuleleChord name="B7" frets={[0, 7, 6, 8]}/>
     </div>,
-    document.querySelector('#root'));
+    document.querySelector('#root')
+);
